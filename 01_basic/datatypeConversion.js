3285fda2f80e72fa9de5b59f5b2080c9a5f77707
ruqiagull2512 <ruqiagull2512@gmail.com>
2025-04-20 15:47:51 +0000

    container config done

---
+++ 01_basic/datatypeConversion.js
@@ -0,0 +1,19 @@
+let score = "33"
+let scores= true
+console.log(typeof scores);
+console.log(typeof score);
+let valueInNumber= Number(score)
+console.log( typeof valueInNumber);
+
+// number conversion 
+//"33"=>33
+//"33abc"=>NAN
+//true=>1 & false =>0
+
+let number="33"
+ 
+let isString= String(number)
+console.log( typeof isString);
+
+
+
