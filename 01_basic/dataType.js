82dc4408a9ea97a0085da6ee629f2db7c4d60442
ruqiagull2512 <ruqiagull2512@gmail.com>
2025-04-20 13:55:11 +0000

    container config done

---
+++ 01_basic/dataType.js
@@ -0,0 +1,24 @@
+//alert(3+3)// we are using node js , not browser
+
+let name="Ruqia"
+let age = 23
+let isLoggedIn= Ture
+let State;
+
+//number => 2 to power 53
+//string =>""
+//boolean =>ture/false
+// null => standalone value(representation of null value)
+// undefined => value not assign to varaiable 
+//symbol => used for uniqueness mostly used in react
+
+
+//Object=>
+
+console.log(typeof "Ruqia");
+console.log(typeof age);
+console.log(typeof State);
+console.log(typeof isLoggedIn);
+
+
+
