26d146b52a87a6e2ebdfaf0739734c5b9a9165e5
ruqiagull2512 <ruqiagull2512@gmail.com>
2025-04-27 08:26:55 +0000

    comparison done

---
+++ 01_basic/comparisonoprtor.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2!=1);
+// console.log(2==1);
+//for comparison used same datatype either string or integer
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//=== , not only check the value but also it datatype
+
+console.log("2"===2);
+
+
+
+
+
+
+
