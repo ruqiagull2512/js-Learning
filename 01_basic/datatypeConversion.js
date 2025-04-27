1e298ed96b38ba09444d3e5042025cf75333918f
ruqiagull2512 <ruqiagull2512@gmail.com>
2025-04-27 12:40:07 +0500

    Update datatypeConversion.js

--- 01_basic/datatypeConversion.js
+++ 01_basic/datatypeConversion.js
@@ -15,5 +15,33 @@ let number="33"
 let isString= String(number)
 console.log( typeof isString);
 
+// operation
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2**2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1="Hello"
+let str2=" Ruqia"
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//if string is in first , whole will considered string (it result will be 122)
+console.log(1+2+"2"); // if string is  in last, first will perform operation then write string(it result will be 32)
+
+let gameCounter=100
+++gameCounter; //pre-operator
+gameCounter++; //post-operator
+console.log(gameCounter);
+
+
 
 
